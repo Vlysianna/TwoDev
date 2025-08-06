b068553847e2a6bc481e696cc8f9a37d7f63ff6a
Ramadan <ramadan260906@gmail.com>
2025-08-06 08:11:58 +0700

    kelola jurusan

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,11 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "../pages/home/tes";
+import LandingPage from "../pages/LandingPage/LandingPage";
+import TentangLSP from "../pages/LandingPage/TentangLSP";
+import Sidebar from "../components/SideAdmin";
+import Test from "../pages/LandingPage/Test";
 
 export default function AppRouter() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/a" element={<Home />} />
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/about" element={<TentangLSP />} />
+				<Route path="/test" element={<Test />} />
 			</Routes>
 		</BrowserRouter>
 	);
