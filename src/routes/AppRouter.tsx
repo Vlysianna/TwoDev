bc3e943e289a81006703931d39e39c236ec5f3db
Kyyet <lukyvirya7@gmail.com>
2025-08-11 20:16:32 +0700

    create dashboard asesor

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "../pages/home/tes";
+import Dashboard from "../pages/Asesor/dashboard";
 
 export default function AppRouter() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/a" element={<Home />} />
+				<Route path="/dashboard-asesor" element={<Dashboard />} />
 			</Routes>
 		</BrowserRouter>
 	);
-}
+}	
