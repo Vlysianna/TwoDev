17246d4afc4b5326fa9b5b4184c7c9fa4e405c54
FajriDarm <fajridarmawan45@gmail.com>
2025-08-11 21:09:06 +0700

    woi

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "../pages/home/tes";
 import Hasil from "@/pages/asesor/Hasil";
+import AssessmentRecord from "@/pages/assesi/AssessmentRecord";
 
 export default function AppRouter() {
 	return (
@@ -8,6 +9,7 @@ export default function AppRouter() {
 			<Routes>
 				<Route path="/a" element={<Home />} />
 				<Route path="/" element={<Hasil />} />
+				<Route path="/aa" element={<AssessmentRecord />} />
 			</Routes>
 		</BrowserRouter>
 	);
