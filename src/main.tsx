fae94c46451999759b702781c77d2331344f1634
condet boy <ojanfauzan81107@gmail.com>
2025-08-15 20:45:29 -0700

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import AppRouter from "./routes/AppRouter.tsx";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<AuthProvider>
-			<App />
-		</AuthProvider>
+		<AppRouter />
 	</StrictMode>
 );
