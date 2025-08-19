d0f022480625f1af7dc352b2c5c022f0790d959d
Kyyet <lukyvirya7@gmail.com>
2025-08-19 07:50:45 +0700

    create dashboard penilaian/asesmen mandiri

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "../pages/home/tes";
+import AsesmenMandiri from "../pages/Asesor/AsesmenMandiri";
+import PersetujuanKerahasiaan from "../pages/Asesor/PersetujuanKerahasiaan";
+import LembarJawaban from "../pages/Asesor/LembarJawaban";
 
 export default function AppRouter() {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/a" element={<Home />} />
+				<Route path="/asesmen-mandiri" element={<AsesmenMandiri />} />
+				<Route path="/persetujuan-kerahasiaan" element={<PersetujuanKerahasiaan />} />
+				<Route path="/lembar-jawaban" element={<LembarJawaban />} />
 			</Routes>
 		</BrowserRouter>
 	);
-}
+}	
